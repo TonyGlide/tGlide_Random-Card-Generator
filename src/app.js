7e2adfd0bcdad22d8e968bd8ed7464d8b29aed44
TonyGlide <162181740+TonyGlide@users.noreply.github.com>
2024-04-15 23:20:43 +0000

    one!

--- src/app.js
+++ src/app.js
@@ -1,11 +1,42 @@
 /* eslint-disable */
 import "bootstrap";
-import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector(".number").innerHTML = values();
+  document.querySelector(".top").innerHTML = sSymbol;
+  document.querySelector(".bottom").innerHTML = sSymbol;
+
+  if (sSymbol === "♦" || sSymbol === "♥") {
+    document.querySelector(".card").style.color = "red";
+  }
 };
+
+function values() {
+  let ArrNum = [
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K",
+    "A"
+  ];
+
+  let numValues = Math.floor(Math.random() * ArrNum.length);
+  return ArrNum[numValues];
+}
+function changeFontColor() {
+  return sSymbol;
+}
+let sSymbol = symbol();
+function symbol() {
+  let ArrSymbol = ["♦", "♥", "♠", "♣"];
+  let sIndex = Math.floor(Math.random() * ArrSymbol.length);
+  return ArrSymbol[sIndex];
+}
