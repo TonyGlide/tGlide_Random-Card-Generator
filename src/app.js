dadd7515ec41f66a1dd572e0a7b86d5a5c5540d2
TonyGlide <162181740+TonyGlide@users.noreply.github.com>
2024-04-15 19:26:27 -0400

    3

--- src/app.js
+++ src/app.js
@@ -12,22 +12,7 @@ window.onload = function() {
 };
 
 function values() {
-  let ArrNum = [
-    "2",
-    "3",
-    "4",
-    "5",
-    "6",
-    "7",
-    "8",
-    "9",
-    "10",
-    "J",
-    "Q",
-    "K",
-    "A"
-  ];
-
+  let ArrNum = [    "2",    "3",    "4",    "5",    "6",    "7",    "8",    "9",    "10",    "J",    "Q",    "K",    "A"  ];
   let numValues = Math.floor(Math.random() * ArrNum.length);
   return ArrNum[numValues];
 }
